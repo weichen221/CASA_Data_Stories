1d1616d8934485018c5aa02e80c5c9d696dbe6f0
weichen221 <w.chen.25@ucl.ac.uk>
2026-03-04 21:54:13 +0000

    update project

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
 
+   base: '/CASA_Data_Stories/', 
+
   plugins: [
     react(),
     tailwindcss()
